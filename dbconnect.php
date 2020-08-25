0a14c10084dc39c37fa7a4e73395acb6e3a85001
jadezhoudo <mr.do036@gmail.com>
2020-08-25 19:00:00 +0700

    aaaa

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,8 @@
 <?php 
 $conn = mysqli_connect('127.0.0.1', 'root', '', 'salomon') or die('khong ket noi duoc');
 
-
+$conn->query("SET NAMES 'utf8mb4'"); 
+$conn->query("SET CHARACTER SET UTF8MB4");  
+$conn->query("SET SESSION collation_connection = 'utf8mb4_unicode_ci'");
 
 ?>
